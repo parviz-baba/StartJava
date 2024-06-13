72a466f3f8e092c378a1d2da72408e3a485beafa
parviz-baba <parviz.babayev1990@gmail.com>
2024-06-13 19:25:12 +0400

    Готов

--- Calculator.java
+++ Calculator.java
@@ -1,46 +1,34 @@
 public class Calculator {
-    public static void main(String[] args) {
+    public static void main(String[]args) {
         int num1 = 2;
         int num2 = 10;
         char operator = '^';
         int result = 0;
+
         if (operator == '+') {
             result = num1 + num2;
-            System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
-            return;
-        } 
-        if (operator == '-') {
+        } else if (operator == '-') {
             result = num1 - num2;
-            System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
-            return;
-        } 
-        if (operator == '*') {
+        } else if (operator == '*') {
             result = num1 * num2;
-            System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
-            return;
-        } 
-        if (operator == '/') {
+        } else if (operator == '/') {
             if (num2 != 0) {
                 result = num1 / num2;
-                System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+            } else {
+                System.out.println("На ноль делить нельзя");
                 return;
-            } 
-            System.out.println("На ноль делить нельзя");
-            return;
-        } 
+
         if (operator == '^') {
             result = 1;
             for (int i = 0; i < num2; i++) {
                 result *= num1;
             }
-            System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
-            return;
-        } 
-        if (operator == '%') {
+        } else if (operator == '%') {
             result = num1 % num2;
-            System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+        } else {
+            System.out.println("Неверный оператор");
             return;
         }
-        System.out.println("Неверный оператор");
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
     }
 }
