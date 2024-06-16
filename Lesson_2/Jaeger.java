2b2f530060b495d04a3bfdc85c15a26ca00cc652
parviz-baba <parviz.babayev1990@gmail.com>
2024-06-16 19:27:42 +0400

    Готов

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,95 @@
+public class Jaeger {
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int strength;
+    private int armor;
+
+    public Jaeger() {
+    }
+
+    public Jaeger(String modelName, String mark, String origin, float height, float weight,
+            int strength, int armor) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+        this.strength = strength;
+        this.armor = armor;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public boolean drift() {
+        return true;
+    }
+
+    public void move() {
+        System.out.println("moving");
+    }
+
+    public String scanKaiju() {
+        return "scanning";
+    }
+
+    public void useVortexCannon() {
+        System.out.println("activated");
+    }
+}
