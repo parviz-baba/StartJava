e8edbda35b3231a1d10f28767e222c7c6e5e80ae
parviz-baba <parviz.babayev1990@gmail.com>
2024-06-19 17:54:40 +0400

    Готов

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,54 +1,34 @@
-public class Calculator {
+class Calculator {
     public double calculate(int num1, String operator, int num2) {
         switch (operator) {
             case "+":
-                return add(num1, num2);
+                return num1 + num2;
             case "-":
-                return subtract(num1, num2);
+                return num1 - num2;
             case "*":
-                return multiply(num1, num2);
+                return num1 * num2;
             case "/":
-                if (num2 == 0) {
-                    System.out.println("Ошибка: деление на ноль запрещено");
+                if (num2 != 0) {
+                    return num1 / (double) num2;
+                } else {
+                    System.out.println("Деление на ноль невозможно.");
                     return Double.NaN;
                 }
-                return divide(num1, num2);
             case "^":
-                return power(num1, num2);
+                return pow(num1, num2);
             case "%":
-                return mod(num1, num2);
+                return num1 % num2;
             default:
-                System.out.println("Ошибка: операция " + operator + " не поддерживается.");
-                System.out.println("Доступны следующие операции: +, -, *, /, ^, %");
+                System.out.println("Неверный оператор.");
                 return Double.NaN;
         }
     }
 
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public double divide(int a, int b) {
-        return (double) a / b;
-    }
-
-    public int power(int a, int b) {
+    public int pow(int a, int b) {
         int result = 1;
         for (int i = 0; i < b; i++) {
             result *= a;
         }
         return result;
-    }
-
-    public int mod(int a, int b) {
-        return a % b;
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean continueCalculating = true;
-        String continueCalculatingChoice;
-
-        while (continueCalculating) {
+        String userChoice;
+        while (true) {
             System.out.println("Введите первое число: ");
             int num1 = Integer.parseInt(scanner.nextLine());
             System.out.println("Введите знак операции (+, -, *, /, ^, %): ");
@@ -15,15 +13,27 @@ public class CalculatorTest {
             int num2 = Integer.parseInt(scanner.nextLine());
             Calculator calculator = new Calculator();
             double result = calculator.calculate(num1, operator, num2);
-            if (!Double.isNaN(result)) {
-                System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
-            }
-            System.out.println("Хотите продолжить вычисления? [yes/no]: ");
-            continueCalculatingChoice = scanner.nextLine();
-            if (continueCalculatingChoice.equalsIgnoreCase("no")) {
-                continueCalculating = false;
+            printResult(num1, operator, num2, result);
+            while (true) {
+                System.out.println("Хотите продолжить вычисления? [yes/no]: ");
+                userChoice = scanner.nextLine();
+                if (userChoice.equalsIgnoreCase("yes")) {
+                    break;
+                } else if (userChoice.equalsIgnoreCase("no")) {
+                    scanner.close();
+                    return;
+                } else {
+                    System.out.println("Пожалуйста, введите 'yes' или 'no'.");
+                }
             }
         }
-        scanner.close();
+    }
+    
+    public static void printResult(int num1, String operator, int num2, double result) {
+        if (!Double.isNaN(result)) {
+            System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+        } else {
+            System.out.println("Ошибка в вычислении.");
+        }
     }
 }
