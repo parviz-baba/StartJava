e9a8feeb178a6be693fba3f261ee409d417e8590
parviz-baba <parviz.babayev1990@gmail.com>
2024-08-31 17:03:44 +0400

    Готов

--- src/com/startjava/lesson_2_3_4/array/Reverser.java
+++ src/com/startjava/lesson_2_3_4/array/Reverser.java
@@ -1,30 +1,27 @@
-public class ArrayReversal {
+public class Reverser {
     public static void main(String[] args) {
-        reverseArray(new int[0]);
-        reverseArray(null);
-        reverseArray(new int[]{6, 8, 9, 1});
-        reverseArray(new int[]{13, 8, 5, 3, 2, 1, 1});
+        toReverse(new int[0]);
+        toReverse(null);
+        toReverse(new int[]{6, 8, 9, 1});
+        toReverse(new int[]{13, 8, 5, 3, 2, 1, 1});
     }
 
-    public static void reverseArray(int[] array) {
+    private static void toReverse(int[] array) {
         if (array == null || array.length == 0) {
             System.out.println("Массив либо нулевой, либо пустой");
             return;
         }
         displayArray("   До реверса: ", array);
-        int left = 0;
-        int right = array.length - 1;
-        while (left < right) {
+        int len = array.length;
+        for (int left = 0, right = len - 1; left < right; left++, right--) {
             int temp = array[left];
             array[left] = array[right];
             array[right] = temp;
-            left++;
-            right--;
         }
         displayArray("После реверса: ", array);
     }
 
-    public static void displayArray(String message, int[] array) {
+    private static void displayArray(String message, int[] array) {
         System.out.print(message + "[");
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
