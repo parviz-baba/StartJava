ba578dab674f75c2929eec04c3fa5704db47da03
parviz-baba <parviz.babayev1990@gmail.com>
2024-09-10 16:41:13 +0400

    готов

--- src/com/startjava/lesson_2_3_4/array/Arrays.java
+++ src/com/startjava/lesson_2_3_4/array/Arrays.java
@@ -1,37 +1,25 @@
 package com.startjava.lesson_2_3_4.array;
 
+import java.util.Random;
+
 public class Arrays {
+    static float[] generateRandomFloats(int lenght) {
+        float[] randomFloat = new float[lenght];
+        Random random = new Random();
+        for (int i = 0; i < randomFloat.length; i++) {
+            randomFloat[i] = random.nextFloat();
+        }
+        return randomFloat;
+    }
+
     static int removeGreaterElements(float[] randomFloats, int address) {
-        double threshold = randomFloats[address];
         int zeroedCount = 0;
         for (int i = 0; i < randomFloats.length; i++) {
-            if (randomFloats[i] > threshold) {
+            if (randomFloats[i] > randomFloats[address]) {
                 randomFloats[i] = 0.0f;
                 zeroedCount++;
             }
         }
         return zeroedCount;
-    }
-
-    static long calculateFactorial(long number) {
-        if (number == 0) return 1;
-        long factorial = 1L;
-        for (long i = 1; i <= number; i++) {
-            factorial *= i;
-        }
-        return factorial;
-    }
-
-    static String formatResult(int number, long result) {
-        if (number == 0) return "0! = 1";
-        StringBuilder resultString = new StringBuilder(number + "! = ");
-        for (int i = 1; i <= number; i++) {
-            resultString.append(i);
-            if (i < number) resultString.append(" * ");
-        }
-        if (number > 1) {
-            resultString.append(" = ").append(result);
-        }
-        return resultString.toString();
     }
 }
--- src/com/startjava/lesson_2_3_4/array/Main.java
+++ src/com/startjava/lesson_2_3_4/array/Main.java
@@ -3,62 +3,23 @@ package com.startjava.lesson_2_3_4.array;
 import static com.startjava.lesson_2_3_4.array.Arrays.*;
 import static com.startjava.lesson_2_3_4.array.Console.*;
 
-import java.util.Random;
-
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        float[] randomFloats = new float[15];
-        Random random = new Random();
+    public static void main(String[] args) {
         int[] addresses = {-1, 15, 0, 14};
         for (int address : addresses) {
-            System.out.println("\nАдрес: " + address);
-            for (int i = 0; i < randomFloats.length; i++) {
-                randomFloats[i] = random.nextFloat();
-            }
-            System.out.println("\nИсходный массив:");
+            printMessage("\nАдрес: " + address + "\n");
+            float[] randomFloats = generateRandomFloats(15);
+            printMessage("Исходный массив:");
             printArray(randomFloats);
             if (address >= 0 && address < randomFloats.length) {
-                double threshold = randomFloats[address];
-                System.out.println("Элемент по указанному адресу: " + address +
-                        ", Значение: " + String.format("%.3f", threshold));
+                printMessage("Элемент по указанному адресу: " + address +
+                        ", Значение: " + String.format("%.3f", randomFloats[address]) +
+                        "\nИзмененный массив:");
                 int zeroedCount = removeGreaterElements(randomFloats, address);
-                System.out.println("Измененный массив:");
                 printArray(randomFloats);
-                System.out.println("Количество обнуленных элементов: " + zeroedCount);
+                printMessage("Количество обнуленных элементов: " + zeroedCount);
             } else {
-                System.out.println("Ошибка: Неверный адрес. За пределами границы массива.");
-            }
-        }
-        printFactorials(new int[]{});
-        printFactorials(null);
-        printFactorials(8, 0, 9);
-        printFactorials(-3, 1, 7, 13);
-        printFactorials(-22, -0);
-        generateNumbers(-10, 20, 23);
-        generateNumbers(-30, 10, 10);
-        generateNumbers(-34, -34, 0);
-        generateNumbers(1, 2, -3);
-        generateNumbers(5, -8, 2);
-        reverse(new int[0]);
-        reverse(null);
-        reverse(new int[]{6, 8, 9, 1});
-        reverse(new int[]{13, 8, 5, 3, 2, 1, 1});
-        printTriangle('0', '9', true);
-        printTriangle('/', '!', false);
-        printTriangle('A', 'J', false);
-        String[] inputs = {
-                "Java - это C++, из которого убрали все пистолеты, ножи и дубинки.\n" +
-                        "- James Gosling",
-                "Чтобы написать чистый код, мы сначала пишем грязный код, затем рефакторим его.\n" +
-                        "- Robert Martin",
-                null,
-                ""
-        };
-        for (String input : inputs) {
-            if (input != null && !input.isEmpty()) {
-                processString(input);
-            } else {
-                System.out.println("Пустая или нулевая строка");
+                System.out.println("Ошибка: Неверный адрес. За пределами границы массива.\n");
             }
         }
     }
