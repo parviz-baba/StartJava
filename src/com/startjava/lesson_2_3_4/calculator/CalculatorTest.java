13f3f4f6709471c51246248362b30d12032913b8
parviz-baba <parviz.babayev1990@gmail.com>
2024-09-14 16:44:21 +0400

    готов

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -6,16 +6,24 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String userChoice = "yes";
-        while (userChoice.equals("yes")) {
+        while (userChoice.equalsIgnoreCase("yes")) {
             System.out.println("Введите математическое выражение: ");
             String expression = scanner.nextLine();
             double result = Calculator.calculate(expression);
-            System.out.println("Результат: " + result);
+            if (!Double.isNaN(result)) {
+                System.out.println("Результат: " + result);
+            } else {
+                System.out.println("Некорректное выражение, попробуйте снова.");
+            }
             System.out.println("Хотите продолжить вычисления? [yes/no]: ");
             userChoice = scanner.nextLine();
-            while (!userChoice.equalsIgnoreCase("yes") && !userChoice.equalsIgnoreCase("no")) {
+            if (!userChoice.equalsIgnoreCase("yes") && !userChoice.equalsIgnoreCase("no")) {
                 System.out.println("Пожалуйста, введите 'yes' или 'no'.");
                 userChoice = scanner.nextLine();
+            }
+            if (userChoice.equalsIgnoreCase("no")) {
+                System.out.println("Вычисления завершены.");
+                break;
             }
         }
     }
