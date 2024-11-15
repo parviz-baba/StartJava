0f2c086d66c533c23051387c765a4a70c8a5be57
parviz-baba <parviz.babayev1990@gmail.com>
2024-11-15 19:59:09 +0400

    готов

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,3 +1,10 @@
+-- Подключение к базе данных robots
+\c robots
+
+-- Удалите существующую таблицу, чтобы можно было создать новую
+DROP TABLE IF EXISTS jaegers;
+
+-- Создание таблицы
 CREATE TABLE IF NOT EXISTS jaegers (
     id SERIAL PRIMARY KEY,
     model_name VARCHAR(50) NOT NULL,
@@ -10,6 +17,8 @@ CREATE TABLE IF NOT EXISTS jaegers (
     kaiju_kill INTEGER
 );
 
+-- Включение файла populate.sql (добавление данных)
 \i D:/git/StartJava/src/com/startjava/lesson_5/populate.sql
 
+-- Включение файла queries.sql (выполнение запросов)
 \i D:/git/StartJava/src/com/startjava/lesson_5/queries.sql
