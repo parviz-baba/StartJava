450f630b24b6bed43d269d157564fcf80e8daa41
parviz-baba <parviz.babayev1990@gmail.com>
2024-11-24 00:47:02 +0400

    ready

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,32 +1,53 @@
 -- Вывести всю таблицу jaegers
-SELECT * FROM jaegers;
+  SELECT j.model_name AS mn, j.mark, j.launch
+  	FROM jaegers AS j;
 
 -- Вывести всех роботов, которые не уничтожены
-SELECT * FROM jaegers WHERE status != 'Destroyed';
+  SELECT j.model_name AS mn, j.mark, j.launch
+    FROM jaegers AS j
+   WHERE j.status != 'Destroyed';
 
 -- Показать только роботов серий Mark-1 и Mark-4
-SELECT * FROM jaegers WHERE mark IN ('Mark-1', 'Mark-4');
+  SELECT j.model_name AS mn, j.mark, j.launch
+    FROM jaegers AS j
+   WHERE j.mark IN ('Mark-1', 'Mark-4');
 
 -- Показать всех роботов, кроме Mark-1 и Mark-4, отсортировав по убыванию столбца mark
-SELECT * FROM jaegers WHERE mark NOT IN ('Mark-1', 'Mark-4') ORDER BY mark DESC;
+  SELECT j.model_name AS mn, j.mark, j.launch
+    FROM jaegers AS j
+   WHERE j.mark NOT IN ('Mark-1', 'Mark-4')
+ORDER BY j.mark DESC;
 
 -- Показать информацию о самом старом роботе
-SELECT * FROM jaegers ORDER BY launch ASC LIMIT 1;
+  SELECT j.model_name AS mn, j.mark, j.launch
+    FROM jaegers AS j
+ORDER BY    j.launch ASC
+LIMIT 1;
 
 -- Показать информацию о роботе, который уничтожил больше всего kaiju
-SELECT model_name, mark, launch, kaiju_kill FROM jaegers ORDER BY kaiju_kill DESC LIMIT 1;
+  SELECT j.model_name AS mn, j.mark, j.launch, j.kaiju_kill
+    FROM jaegers AS j
+ORDER BY j.kaiju_kill DESC
+LIMIT 1;
 
 -- Вывести средний вес роботов, округлив до трёх знаков после запятой
-SELECT ROUND(AVG(weight), 3) AS avg_weight FROM jaegers;
+  SELECT ROUND(AVG(j.weight), 3) AS avg_weight
+    FROM jaegers AS j;
 
 -- Увеличить количество уничтоженных kaiju для уничтоженных роботов на единицу
-UPDATE jaegers SET kaiju_kill = kaiju_kill + 1 WHERE status = 'Destroyed';
+  UPDATE jaegers AS j
+     SET j.kaiju_kill = j.kaiju_kill + 1
+   WHERE j.status = 'Destroyed';
 
 -- Показать обновлённую таблицу
-SELECT * FROM jaegers;
+  SELECT j.model_name AS mn, j.mark, j.launch
+    FROM jaegers AS j;
 
 -- Удалить уничтоженных роботов
-DELETE FROM jaegers WHERE status = 'Destroyed';
+DELETE
+  	FROM jaegers AS j
+   WHERE j.status = 'Destroyed';
 
 -- Показать оставшихся роботов
-SELECT * FROM jaegers;
+  SELECT j.model_name AS mn, j.mark, j.launch
+    FROM jaegers AS j;
