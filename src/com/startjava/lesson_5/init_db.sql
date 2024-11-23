3b8be4accf26cf90cff65fedffbdd9c5dac351ba
parviz-baba <parviz.babayev1990@gmail.com>
2024-11-24 01:39:15 +0400

    ready

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -6,15 +6,15 @@ DROP TABLE IF EXISTS jaegers;
 
 -- Создать таблицу jaegers, если она ещё не существует
 CREATE TABLE IF NOT EXISTS jaegers (
-    jaeger_id     SERIAL       PRIMARY KEY,
-    model_name    VARCHAR(50)  NOT NULL,
-    mark          VARCHAR(50)  NOT NULL,
-    height        REAL         NOT NULL,
-    weight        REAL         NOT NULL,
-    status        VARCHAR(20),
-    origin        VARCHAR(50),
-    launch        DATE,
-    kaiju_kill    INTEGER
+    jaeger_id  SERIAL       PRIMARY KEY,
+    model_name VARCHAR(50)  NOT NULL,
+    mark       VARCHAR(50)  NOT NULL,
+    height     REAL         NOT NULL,
+    weight     REAL         NOT NULL,
+    status     VARCHAR(20),
+    origin     VARCHAR(50),
+    launch     DATE,
+    kaiju_kill INTEGER
 );
 
 -- Включение файла для добавления данных (populate.sql)
