5d543992e7a9f78d65d3a3f347b40e7b63af4068
parviz-baba <157913708+parviz-baba@users.noreply.github.com>
2024-11-24 08:30:03 +0400

    Update queries.sql

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,32 +1,32 @@
 -- Вывести всю таблицу jaegers
-  SELECT j.model_name AS mn, j.mark, j.launch
-  	FROM jaegers AS j;
+  SELECT j.id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+  	 FROM jaegers AS j;
 
 -- Вывести всех роботов, которые не уничтожены
-  SELECT j.model_name AS mn, j.mark, j.launch
-    FROM jaegers AS j
+  SELECT j.id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+    FROM jaegers AS j;
    WHERE j.status != 'Destroyed';
 
 -- Показать только роботов серий Mark-1 и Mark-4
-  SELECT j.model_name AS mn, j.mark, j.launch
-    FROM jaegers AS j
+  SELECT j.id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+    FROM jaegers AS j;
    WHERE j.mark IN ('Mark-1', 'Mark-4');
 
 -- Показать всех роботов, кроме Mark-1 и Mark-4, отсортировав по убыванию столбца mark
-  SELECT j.model_name AS mn, j.mark, j.launch
-    FROM jaegers AS j
+  SELECT j.id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+    FROM jaegers AS j;
    WHERE j.mark NOT IN ('Mark-1', 'Mark-4')
 ORDER BY j.mark DESC;
 
 -- Показать информацию о самом старом роботе
-  SELECT j.model_name AS mn, j.mark, j.launch
-    FROM jaegers AS j
+  SELECT j.id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+    FROM jaegers AS j;
 ORDER BY    j.launch ASC
 LIMIT 1;
 
 -- Показать информацию о роботе, который уничтожил больше всего kaiju
-  SELECT j.model_name AS mn, j.mark, j.launch, j.kaiju_kill
-    FROM jaegers AS j
+  SELECT j.id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+    FROM jaegers AS j;
 ORDER BY j.kaiju_kill DESC
 LIMIT 1;
 
@@ -40,14 +40,14 @@ LIMIT 1;
    WHERE status = 'Destroyed';
 
 -- Показать обновлённую таблицу
-  SELECT j.model_name AS mn, j.mark, j.launch
-    FROM jaegers AS j;
+  SELECT j.id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+  	 FROM jaegers AS j;
 
 -- Удалить уничтоженных роботов
 DELETE
-  	FROM jaegers AS j
+   	FROM jaegers AS j
    WHERE j.status = 'Destroyed';
 
 -- Показать оставшихся роботов
-  SELECT j.model_name AS mn, j.mark, j.launch
-    FROM jaegers AS j;
+  SELECT j.id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+  	 FROM jaegers AS j;
