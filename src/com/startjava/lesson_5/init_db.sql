680bd463a7b98d9c1a8c242fdcd619a16297720b
parviz-baba <parviz.babayev1990@gmail.com>
2024-11-24 15:52:05 +0400

    ready

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -5,20 +5,18 @@
 DROP TABLE IF EXISTS jaegers;
 
 -- Создать таблицу jaegers, если она ещё не существует
-CREATE TABLE IF NOT EXISTS jaegers (
+CREATE TABLE jaegers (
     jaeger_id  SERIAL       PRIMARY KEY,
     model_name VARCHAR(50)  NOT NULL,
     mark       VARCHAR(50)  NOT NULL,
-    height     REAL         NOT NULL,
-    weight     REAL         NOT NULL,
+    height     NUMERIC         NOT NULL,
+    weight     NUMERIC         NOT NULL,
     status     VARCHAR(20),
     origin     VARCHAR(50),
     launch     DATE,
     kaiju_kill INTEGER
 );
 
--- Включение файла для добавления данных (populate.sql)
-\i D:/git/StartJava/src/com/startjava/lesson_5/populate.sql
-
--- Включение файла для выполнения запросов (queries.sql)
-\i D:/git/StartJava/src/com/startjava/lesson_5/queries.sql
+-- Подключение файлов с использованием двух ключей (include и relative)
+\ir populate.sql
+\ir queries.sql
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,37 +1,40 @@
 -- Вывести всю таблицу jaegers
-SELECT j.jaeger_id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
-  FROM jaegers AS j;
+SELECT *, j.model_name AS mn, j.kaiju_kill AS kk
+  FROM jaegers AS j
+ ORDER BY j.model_name ASC;
 
 -- Вывести всех роботов, которые не уничтожены
-SELECT j.jaeger_id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+SELECT *, j.model_name AS mn, j.kaiju_kill AS kk
   FROM jaegers AS j
- WHERE j.status != 'Destroyed';
+ WHERE j.status != 'Destroyed'
+ ORDER BY j.model_name ASC;
 
 -- Показать только роботов серий Mark-1 и Mark-4
-SELECT j.jaeger_id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+SELECT *, j.model_name AS mn, j.kaiju_kill AS kk
   FROM jaegers AS j
- WHERE j.mark IN ('Mark-1', 'Mark-4');
+ WHERE j.mark IN ('Mark-1', 'Mark-4')
+ ORDER BY j.model_name ASC;
 
 -- Показать всех роботов, кроме Mark-1 и Mark-4, отсортировав по убыванию столбца mark
-SELECT j.jaeger_id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+SELECT *, j.model_name AS mn, j.kaiju_kill AS kk
   FROM jaegers AS j
  WHERE j.mark NOT IN ('Mark-1', 'Mark-4')
- ORDER BY j.mark DESC;
+ ORDER BY j.mark DESC, j.model_name ASC;
 
 -- Показать информацию о самом старом роботе
-SELECT j.jaeger_id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+SELECT *, j.model_name AS mn, j.kaiju_kill AS kk
   FROM jaegers AS j
  ORDER BY j.launch ASC
  LIMIT 1;
 
 -- Показать информацию о роботе, который уничтожил больше всего kaiju
-SELECT j.jaeger_id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
+SELECT *, j.model_name AS mn, j.kaiju_kill AS kk
   FROM jaegers AS j
  ORDER BY j.kaiju_kill DESC
  LIMIT 1;
 
 -- Вывести средний вес роботов, округлив до трёх знаков после запятой
-SELECT ROUND(CAST(AVG(j.weight) AS NUMERIC), 3) AS avg_weight
+SELECT ROUND(AVG(j.weight), 3) AS avg_weight
   FROM jaegers AS j;
 
 -- Увеличить количество уничтоженных kaiju для уничтоженных роботов на единицу
@@ -40,13 +43,15 @@ UPDATE jaegers
  WHERE status = 'Destroyed';
 
 -- Показать обновлённую таблицу
-SELECT j.jaeger_id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
-  FROM jaegers AS j;
+SELECT *, j.model_name AS mn, j.kaiju_kill AS kk
+  FROM jaegers AS j
+ ORDER BY j.model_name ASC;
 
 -- Удалить уничтоженных роботов
 DELETE FROM jaegers
  WHERE status = 'Destroyed';
 
 -- Показать оставшихся роботов
-SELECT j.jaeger_id, j.model_name AS mn, j.mark, j.height, j.weight, j.status, j.origin, j.launch, j.kaiju_kill AS kk
-  FROM jaegers AS j;
+SELECT *, j.model_name AS mn, j.kaiju_kill AS kk
+  FROM jaegers AS j
+ ORDER BY j.model_name ASC;
